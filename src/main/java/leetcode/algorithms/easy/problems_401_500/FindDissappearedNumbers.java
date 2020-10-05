bec774c09a20784e1327663565f9a1a4b797fc9e
Rajendran <rraj91091@gmail.com>
2020-10-05 14:11:19 +0800

    added v2 solution for Dissappeared Numbers in String

--- src/main/java/leetcode/algorithms/easy/problems_401_500/FindDissappearedNumbers.java
+++ src/main/java/leetcode/algorithms/easy/problems_401_500/FindDissappearedNumbers.java
@@ -36,4 +36,26 @@ public class FindDissappearedNumbers {
 
         return result;
     }
+
+    /**
+     * Uses no auxiliary array, but slightly longer execution time.
+     */
+    public List<Integer> findDisappearedNumbersV2(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int index = Math.abs(nums[i]);
+            if(nums[index-1] > 0) {
+                nums[index-1] *= -1;
+            }
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                result.add(i+1);
+            }
+        }
+
+        return result;
+    }
 }
