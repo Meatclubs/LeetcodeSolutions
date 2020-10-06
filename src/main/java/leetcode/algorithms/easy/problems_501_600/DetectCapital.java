499ab782e9deefae2767382d76282d9488abc57e
Rajendran <rraj91091@gmail.com>
2020-10-06 20:58:14 +0800

    solution for detect capital

---
+++ src/main/java/leetcode/algorithms/easy/problems_501_600/DetectCapital.java
@@ -0,0 +1,53 @@
+package main.java.leetcode.algorithms.easy.problems_501_600;
+
+/**
+ * Given a word, you need to judge whether the usage of capitals in it is right or not.
+ * We define the usage of capitals in a word to be right when one of the following cases holds:
+ * All letters in this word are capitals, like "USA".
+ * All letters in this word are not capitals, like "leetcode".
+ * Only the first letter in this word is capital, like "Google".
+ * Otherwise, we define that this word doesn't use capitals in a right way.
+ *
+ * Example 1:
+ * Input: "USA"
+ * Output: True
+ *
+ * Example 2:
+ * Input: "FlaG"
+ * Output: False
+ *
+ * Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+ */
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        boolean isFirstCapital = false;
+        boolean isAllCapital = false;
+        int capitals = 0;
+
+        for(int i=0; i<word.length(); i++) {
+            if(i == 0) {
+                if(word.charAt(i) < 'a') {
+                    isFirstCapital = true;
+                    isAllCapital = true;
+                    capitals++;
+                    continue;
+                }
+            }
+
+            if(word.charAt(i) < 'a') {
+                capitals++;
+                if(!isAllCapital) {
+                    return false;
+                }
+            } else {
+                isAllCapital = false;
+            }
+        }
+
+        if(!isAllCapital && capitals > 1) {
+            return false;
+        }
+
+        return true;
+    }
+}
