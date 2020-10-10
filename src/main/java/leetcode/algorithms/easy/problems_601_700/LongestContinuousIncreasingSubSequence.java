4eb6ad6d2f5996a49a6a830b66d386f0b2cbf88b
Rajendran <rraj91091@gmail.com>
2020-10-10 21:27:26 +0800

    Longest continuous increase subsequence

---
+++ src/main/java/leetcode/algorithms/easy/problems_601_700/LongestContinuousIncreasingSubSequence.java
@@ -0,0 +1,39 @@
+package main.java.leetcode.algorithms.easy.problems_601_700;
+
+/**
+ * Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+ *
+ * Example 1:
+ * Input: [1,3,5,4,7]
+ * Output: 3
+ * Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3.
+ * Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4.
+ *
+ * Example 2:
+ * Input: [2,2,2,2,2]
+ * Output: 1
+ * Explanation: The longest continuous increasing subsequence is [2], its length is 1.
+ *
+ * Note: Length of the array will not exceed 10,000.
+ */
+public class LongestContinuousIncreasingSubSequence {
+    public int findLengthOfLCIS(int[] nums) {
+        if(nums.length < 2) { return nums.length; }
+
+        int length = 1;
+        int start = 0;
+        int end = 1;
+
+        while(end < nums.length) {
+            if(nums[end-1] >= nums[end]) {
+                length = Math.max(length, end-start);
+                start = end;
+            }
+            end++;
+        }
+
+        length = Math.max(length, end-start);
+
+        return length;
+    }
+}
