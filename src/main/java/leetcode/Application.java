21e2e98b1defcd3ef2db04763b1ec54b143083ae
Rajendran <rraj91091@gmail.com>
2020-10-11 19:13:33 +0800

    solution for binary number with alternating bits

--- src/main/java/leetcode/Application.java
+++ src/main/java/leetcode/Application.java
@@ -1,8 +1,18 @@
 package main.java.leetcode;
 
+import javax.swing.*;
+
 public class Application {
 
     public static void main(String[] args) {
+        int x = 56;
+
+        //x--;
+        x |= x >> 1;
+        System.out.println(x);
+        //x++;
+
+        System.out.println(x);
 
     }
 
