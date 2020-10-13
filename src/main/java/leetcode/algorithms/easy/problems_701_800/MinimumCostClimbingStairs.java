56492756f28163ccfe0f0d1e053bf3796d258527
Rajendran <rraj91091@gmail.com>
2020-10-13 16:52:49 +0800

    solution for largest number twice of others

--- src/main/java/leetcode/algorithms/easy/problems_701_800/MinimumCostClimbingStairs.java
+++ src/main/java/leetcode/algorithms/easy/problems_701_800/MinimumCostClimbingStairs.java
@@ -14,7 +14,7 @@ package main.java.leetcode.algorithms.easy.problems_701_800;
  * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
  * Output: 6
  * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
- * 
+ *
  * Note:
  * cost will have a length in the range [2, 1000].
  * Every cost[i] will be an integer in the range [0, 999].
