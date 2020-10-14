c4abc692dcad220aee65a2eb79809747ca4ee3c3
Rajendran <rraj91091@gmail.com>
2020-10-14 16:36:15 +0800

    New O(1) space solution for Backspace String Compare

--- src/main/java/leetcode/algorithms/easy/problems_801_900/BackspaceStringCompare.java
+++ src/main/java/leetcode/algorithms/easy/problems_801_900/BackspaceStringCompare.java
@@ -28,7 +28,7 @@ package main.java.leetcode.algorithms.easy.problems_801_900;
  * 1 <= S.length <= 200
  * 1 <= T.length <= 200
  * S and T only contain lowercase letters and '#' characters.
- * 
+ *
  * Follow up:
  * Can you solve it in O(N) time and O(1) space?
  */
@@ -58,4 +58,51 @@ public class BackspaceStringCompare {
         }
         return sb.reverse().toString();
     }
+
+    /**
+     * O(1) additional space
+     */
+    public boolean backspaceCompareV2(String S, String T) {
+        int sss = S.length()-1;
+        int ttt = T.length()-1;
+        int sbs = 0;
+        int tbs = 0;
+
+        while(sss >=0 || ttt >= 0) {
+            while(sss >= 0) {
+                if(S.charAt(sss) == '#') {
+                    sbs++;
+                    sss--;
+                } else if(sbs > 0) {
+                    sbs--;
+                    sss--;
+                } else {
+                    break;
+                }
+            }
+
+            while(ttt >= 0) {
+                if(T.charAt(ttt) == '#') {
+                    tbs++;
+                    ttt--;
+                } else if(tbs > 0) {
+                    tbs--;
+                    ttt--;
+                } else {
+                    break;
+                }
+            }
+
+            //if 1 string is traversed and the other is not yet finished
+            if((sss < 0) != (ttt < 0)) return false;
+
+            if(sss >= 0 && ttt >= 0 && S.charAt(sss) != T.charAt(ttt)) return false;
+
+            ttt--;
+            sss--;
+        }
+
+        return true;
+    }
+
 }
