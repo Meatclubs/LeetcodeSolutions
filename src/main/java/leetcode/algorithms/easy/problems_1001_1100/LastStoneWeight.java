6e7876419dd31b0702efee13f6c169b8d3d8b1d8
Rajendran <rraj91091@gmail.com>
2020-10-19 13:16:39 +0800

    Last Stone Weight

---
+++ src/main/java/leetcode/algorithms/easy/problems_1001_1100/LastStoneWeight.java
@@ -0,0 +1,46 @@
+package main.java.leetcode.algorithms.easy.problems_1001_1100;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * We have a collection of stones, each stone has a positive integer weight.
+ * Each turn, we choose the two heaviest stones and smash them together.
+ * Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+ *
+ * If x == y, both stones are totally destroyed;
+ * If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+ * At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+ *
+ * Example 1:
+ * Input: [2,7,4,1,8,1]
+ * Output: 1
+ * Explanation:
+ * We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+ * we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+ * we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+ * we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+ *
+ * Note:
+ * 1 <= stones.length <= 30
+ * 1 <= stones[i] <= 1000
+ */
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int stone : stones) heap.offer(stone);
+
+        while(heap.size() > 1) {
+            int stone1 = heap.poll();
+            int stone2 = heap.poll();
+
+            if(stone1 - stone2 > 0) heap.offer(stone1 - stone2);
+        }
+
+        if(heap.isEmpty()) return 0;
+
+        return heap.poll();
+    }
+}
